8cef701371b7046165b6f8d4fe40a0569205bf8d
Jaehwan Ryu <time0121@gmail.com>
2015-02-03 18:31:09 -0500

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 'use strict';
 /*jshint node: true */
 
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-// var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var inject = require('gulp-inject');
-var sh = require('shelljs');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    bower = require('bower'),
+    // concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    inject = require('gulp-inject'),
+    sh = require('shelljs');
 
 var paths = {
   sass: ['./www/app/**/*.scss']
@@ -17,16 +17,15 @@ var paths = {
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function (done) {
-  gulp.src('./www/app/*.scss')
+gulp.task('sass', function () {
+  return gulp.src('./www/app/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    .pipe(gulp.dest('./www/css/'));
 });
 
 gulp.task('index', function () {
